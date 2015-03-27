b3e6c03b86ada28da01701947765f229305a21e3
Ajit Kumar Mohapatra <mohapatr@lxplus0196.cern.ch>
2015-03-27 17:55:41 +0100

    Information about T2 -> T1 links

--- mc/getuplinks.py
+++ mc/getuplinks.py
@@ -50,6 +50,7 @@ def human(n):
 	letter = {1:'k',2:'M',3:'G',4:'T'}
 	return ("%.1f%s" % (value,letter[order])).replace(".0", "")
 
+# Main
 def main():
 	global blacklist
 	mcdir = '/afs/cern.ch/user/c/cmst2/www/mc'
